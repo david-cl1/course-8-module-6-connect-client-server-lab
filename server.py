4680cf4d2b133e4c4aa8e634a2bf04421a72c4f9
david-cl1 <crowndavid32@gmail.com>
2026-08-24 20:07:11 +0300

    submission

--- server.py
+++ server.py
@@ -6,12 +6,22 @@ CORS(app)
 
 # Create a list called 'events' with a couple of sample event dictionaries
 # Each dictionary should have an 'id' and a 'title'
+events = [{'id':1,
+           'title':"school event"},
+           {'id':2,
+           'title':"church event"}]
 
 # TASK: Create a route for "/"
 # This route should return a JSON welcome message
+@app.route ('/')
+def home():
+    return {'message':"welcome"}
 
 # TASK: Create a GET route for "/events"
 # This route should return the full list of events as JSON
+@app.route('/events')
+def all_events ():
+    return events.jsonify
 
 # TASK: Create a POST route for "/events"
 # This route should:
@@ -20,6 +30,16 @@ CORS(app)
 # 3. Create a new event with a unique ID and the provided title
 # 4. Add the new event to the events list
 # 5. Return the new event with status code 201
+@app.route('/events', methods=["POST"])
+def add_event():
+    new_id = len(events) +1
+    data = request.get_json()
+    new_title = data.get('title')
 
+    new_event = {'id': f"{new_id}",
+                 'title':f"{new_title}"}
+
+    events.append(new_event)
+    return new_event ,201
 if __name__ == "__main__":
     app.run(debug=True)
